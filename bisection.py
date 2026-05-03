4b4aa130a88098907524416159dbaa2f21ee3a0d
negiakki <139623842+negiakki@users.noreply.github.com>
2026-05-03 20:57:50 +0530

    Implement bisection method for root finding

--- bisection.py
+++ bisection.py
@@ -1,1 +1,19 @@
+def f(x):
+    return x**3 - x - 2
 
+def bisection(a, b, tol=1e-6):
+    if f(a) * f(b) >= 0:
+        print("Invalid interval")
+        return
+
+    while (b - a) / 2 > tol:
+        c = (a + b) / 2
+        if f(c) == 0:
+            return c
+        elif f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+    return (a + b) / 2
+
+print(bisection(1, 2))
